72b071e444c5a81c52b20e73d6956ff60de81587
swan28pande <190020119@iitb.ac.in>
2021-06-19 17:30:33 +0530

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';   
-import 	BasicTable from './AppComponents/BasicTable' 
+import 	BasicTable from './AppComponents/BasicTable'; 
+import Navbar from './AppComponents/Navbar/Navbar';
+import './App.css';
+
 
 function App(){
 	return (  
         <div classname= 'App'>
 
+            <Navbar />
         	<BasicTable />     
             
         </div>        
