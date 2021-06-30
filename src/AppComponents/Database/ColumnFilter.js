7f39d702d0ef0a53875e856728914445d0b5ccbd
MananMohnot <manan.s.mohnot@gmail.com>
2021-06-30 16:35:19 +0530

    Added filters

---
+++ src/AppComponents/Database/ColumnFilter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const ColumnFilter = ({ column }) => {
+    const {filterValue, setFilter} = column
+    return (
+        <span>
+            
+            {' '}
+            <input value= {filterValue || ''} onChange = {(e) => setFilter(e.target.value)}/>
+        </span>
+    )
+}
