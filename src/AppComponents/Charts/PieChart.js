c77862be892bcfc8e63b7e05519401d15a81a53c
dev9318 <dev.moxaj@gmail.com>
2021-07-05 23:49:23 +0530

    working pie chart

--- src/AppComponents/Charts/PieChart.js
+++ src/AppComponents/Charts/PieChart.js
@@ -33,64 +33,40 @@ function Piechart(){
     const [error, setError] = useState(null);
     const url = 'https://chemdbsurp.herokuapp.com/group?group=Type';
 
-    // useEffect(() => {
-    //     const abortCont = new AbortController();
-    //     fetch(url, {signal: abortCont.signal})
-    //     .then(res => {
-    //         if (!res.ok) { // error coming back from server
-    //         throw Error('could not fetch the data for that resource');
-    //         } 
-    //         return res.json();
-    //     })
-    //     .then(res => {
-    //         setIsPending(false);
-    //         setData([
-    //             {
-    //               "name": "Group A",
-    //               "value": 400
-    //             },
-    //             {
-    //               "name": "Group B",
-    //               "value": 300
-    //             },
-    //             {
-    //               "name": "Group C",
-    //               "value": 300
-    //             },
-    //             {
-    //               "name": "Group D",
-    //               "value": 200
-    //             },
-    //             {
-    //               "name": "Group E",
-    //               "value": 278
-    //             },
-    //             {
-    //               "name": "Group F",
-    //               "value": 189
-    //             }
-    //           ]);
-    //         console.log(data);
-    //         setError('error');
-    //     })
-    //     .catch(err => {
-    //         // auto catches network / connection error
-    //     if (err.name === 'AbortError'){
-    //         console.log("Fetch Aborted");
-    //     }
-    //     else{
-    //         setIsPending(false);
-    //         setError(err.message);
-    //     }
-    //     })
-    //   }, [url]);
+    useEffect(() => {
+        const abortCont = new AbortController();
+        fetch(url, {signal: abortCont.signal})
+        .then(res => {
+            if (!res.ok) { // error coming back from server
+            throw Error('could not fetch the data for that resource');
+            } 
+            return res.json();
+        })
+        .then(res => {
+            setIsPending(false);
+            setData(res.data);
+            setError('error');
+        })
+        .catch(err => {
+            // auto catches network / connection error
+        if (err.name === 'AbortError'){
+            console.log("Fetch Aborted");
+        }
+        else{
+            setIsPending(false);
+            setError(err.message);
+        }
+        })
+      }, []);
 
     return (
-        <ResponsiveContainer width={700} height="80%">
-                <PieChart width={300} height="40%">
-                    <Pie data={data} dataKey="value" nameKey="name" />
-                </PieChart>
-            </ResponsiveContainer>
+        
+      <center> 
+        <PieChart width={700} height={700}>
+          <Pie data={data} dataKey="count" nameKey="_id" label="_id"/>
+        </PieChart>
+      </center>
+            
     );
                  
     
