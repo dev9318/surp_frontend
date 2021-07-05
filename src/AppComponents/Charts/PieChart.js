39e432caea830c26fdcc685c39a78ca47c2afa63
dev9318 <dev.moxaj@gmail.com>
2021-07-06 00:39:44 +0530

    add a button to control the type of column

--- src/AppComponents/Charts/PieChart.js
+++ src/AppComponents/Charts/PieChart.js
@@ -3,37 +3,15 @@ import { PieChart, Pie, ResponsiveContainer } from 'recharts';
 import { useState, useEffect } from 'react';
 
 function Piechart(){
-    var [data, setData] = useState([
-                    {
-                      "name": "Group A",
-                      "value": 400
-                    },
-                    {
-                      "name": "Group B",
-                      "value": 300
-                    },
-                    {
-                      "name": "Group C",
-                      "value": 300
-                    },
-                    {
-                      "name": "Group D",
-                      "value": 200
-                    },
-                    {
-                      "name": "Group E",
-                      "value": 278
-                    },
-                    {
-                      "name": "Group F",
-                      "value": 189
-                    }
-                  ]);
+    var [data, setData] = useState([]);
     var [pending, setIsPending] = useState(false);
     const [error, setError] = useState(null);
-    const url = 'https://chemdbsurp.herokuapp.com/group?group=Type';
+    const [open, setOpen] = useState(false);
+    const [col, setCol] = useState('Type');
+    const columns = ['Type', 'Date', 'Company']
 
     useEffect(() => {
+        const url = `https://chemdbsurp.herokuapp.com/group?group=${col}`;
         const abortCont = new AbortController();
         fetch(url, {signal: abortCont.signal})
         .then(res => {
@@ -57,15 +35,31 @@ function Piechart(){
             setError(err.message);
         }
         })
-      }, []);
+      }, [col]);
+
 
     return (
-        
-      <center> 
-        <PieChart width={700} height={700}>
-          <Pie data={data} dataKey="count" nameKey="_id" label="_id"/>
-        </PieChart>
-      </center>
+        <div>
+          <button onClick={()=> setOpen(open=>!open)}> {col} </button>
+          {open && <div className="shadow h-auto w-56">
+          <ul className="text-left">
+            {columns.map((item, i) => (
+                <li key={i} style={{"background":"#ff9999","margin": "1px", "padding": "5px","position": "flex"}} onClick={() => {
+                  setOpen(false);
+                  setCol(item);
+                }}>
+                  {item}
+                </li>
+              ))}
+          </ul>
+        </div>}
+         <center> 
+          <PieChart width={700} height={700}>
+            <Pie data={data} dataKey="count" nameKey="_id" label="_id"/>
+          </PieChart>
+        </center>
+      </div>  
+       
             
     );
                  
